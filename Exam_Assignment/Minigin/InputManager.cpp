7e5eebcaaa9f07d30cef0250743fb327f4257b03
Laptop Dylan <Dylan.millian@hotmail.com>
2019-05-11 12:59:19 +0200

    Refactoring InputManager

--- Exam_Assignment/Minigin/InputManager.cpp
+++ Exam_Assignment/Minigin/InputManager.cpp
@@ -3,6 +3,11 @@
 #include <SDL.h>
 
 
+dae::InputManager::~InputManager()
+{
+}
+
+
 bool dae::InputManager::ProcessInput(float deltatime)
 {
 	for (int i{}; i < m_MAXammControllers; i++)
@@ -27,171 +32,35 @@ bool dae::InputManager::ProcessInput(float deltatime)
 		}
 		if (IsPressed(ControllerButton::ButtonA))
 		{
-			switch (i)
-			{
-			case 0:
-				if (aBtnCommand_One != nullptr) aBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (aBtnCommand_Two != nullptr) aBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (aBtnCommand_Three != nullptr) aBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (aBtnCommand_Four != nullptr) aBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}	
+			if (aBtnCommand[i] != nullptr) aBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::ButtonB))
 		{
-			switch (i)
-			{
-			case 0:
-				if (bBtnCommand_One != nullptr) bBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (bBtnCommand_Two != nullptr) bBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (bBtnCommand_Three != nullptr) bBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (bBtnCommand_Four != nullptr) bBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}			
+			if (bBtnCommand[i] != nullptr) bBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::ButtonX))
 		{
-			switch (i)
-			{
-			case 0:
-				if (xBtnCommand_One != nullptr) xBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (xBtnCommand_Two != nullptr) xBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (xBtnCommand_Three != nullptr) xBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (xBtnCommand_Four != nullptr) xBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}
+			if (xBtnCommand[i] != nullptr) xBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::ButtonY))
 		{
-			switch (i)
-			{
-			case 0:
-				if (yBtnCommand_One != nullptr) yBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (yBtnCommand_Two != nullptr) yBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (yBtnCommand_Three != nullptr) yBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (yBtnCommand_Four != nullptr) yBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}
+			if (yBtnCommand[i] != nullptr) yBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::DpadL))
 		{
-			switch (i)
-			{
-			case 0:
-				if (padlBtnCommand_One != nullptr) padlBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (padlBtnCommand_Two != nullptr) padlBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (padlBtnCommand_Three != nullptr) padlBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (padlBtnCommand_Four != nullptr) padlBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}
+			if (padlBtnCommand[i] != nullptr) padlBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::DpadR))
 		{
-			switch (i)
-			{
-			case 0:
-				if (padrBtnCommand_One != nullptr) padrBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (padrBtnCommand_Two != nullptr) padrBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (padrBtnCommand_Three != nullptr) padrBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (padrBtnCommand_Four != nullptr) padrBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}
+			if (padrBtnCommand[i] != nullptr) padrBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::DpadU))
 		{
-			switch (i)
-			{
-			case 0:
-				if (paduBtnCommand_One != nullptr) paduBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (paduBtnCommand_Two != nullptr) paduBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (paduBtnCommand_Three != nullptr) paduBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (paduBtnCommand_Four != nullptr) paduBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}
+			if (paduBtnCommand[i] != nullptr) paduBtnCommand[i]->Execute(deltatime);
 		}
 		if (IsPressed(ControllerButton::DpadD))
 		{
-			switch (i)
-			{
-			case 0:
-				if (paddBtnCommand_One != nullptr) paddBtnCommand_One->Execute(deltatime);
-				break;
-			case 1:
-				if (paddBtnCommand_Two != nullptr) paddBtnCommand_Two->Execute(deltatime);
-				break;
-			case 2:
-				if (paddBtnCommand_Three != nullptr) paddBtnCommand_Three->Execute(deltatime);
-				break;
-			case 3:
-				if (paddBtnCommand_Four != nullptr) paddBtnCommand_Four->Execute(deltatime);
-				break;
-			default:
-				return false;
-				break;
-			}
+			if (paddBtnCommand[i] != nullptr) paddBtnCommand[i]->Execute(deltatime);
 		}
 	}	
 	return true;
@@ -226,52 +95,28 @@ void dae::InputManager::ChangeCommand(ControllerButton button, int controller, C
 	switch (button)
 	{
 	case dae::ControllerButton::ButtonA:
-		if (controller == 0) aBtnCommand_One = newcommand;
-			if (controller == 1) aBtnCommand_Two = newcommand;
-				if (controller == 2) aBtnCommand_Three = newcommand;
-					if(controller == 3) aBtnCommand_Four = newcommand;
+		aBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::ButtonB:
-		if (controller == 0) bBtnCommand_One = newcommand;
-			if (controller == 1) bBtnCommand_Two = newcommand;
-				if (controller == 2) bBtnCommand_Three = newcommand;
-					if(controller == 3) bBtnCommand_Four = newcommand;
+		bBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::ButtonX:
-		if (controller == 0) xBtnCommand_One = newcommand;
-			if (controller == 1) xBtnCommand_Two = newcommand;
-				if (controller == 2) xBtnCommand_Three = newcommand;
-					if(controller == 3) xBtnCommand_Four = newcommand;
+		xBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::ButtonY:
-		if (controller == 0) yBtnCommand_One = newcommand;
-			if (controller == 1) yBtnCommand_Two = newcommand;
-				if (controller == 2) yBtnCommand_Three = newcommand;
-					if(controller == 3) yBtnCommand_Four = newcommand;
+		yBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::DpadL:
-		if (controller == 0) padlBtnCommand_One = newcommand;
-			if (controller == 1) padlBtnCommand_Two = newcommand;
-				if (controller == 2) padlBtnCommand_Three = newcommand;
-					if(controller == 3) padlBtnCommand_Four = newcommand;
+		padlBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::DpadR:
-		if (controller == 0) padrBtnCommand_One = newcommand;
-			if (controller == 1) padrBtnCommand_Two = newcommand;
-				if (controller == 2) padrBtnCommand_Three = newcommand;
-					if(controller == 3) padrBtnCommand_Four = newcommand;
+		padrBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::DpadU:
-		if (controller == 0) paduBtnCommand_One = newcommand;
-			if (controller == 1) paduBtnCommand_Two = newcommand;
-				if (controller == 2) paduBtnCommand_Three = newcommand;
-					if(controller == 3) paduBtnCommand_Four = newcommand;
+		paduBtnCommand[controller] = newcommand;
 		break;
 	case dae::ControllerButton::DpadD:
-		if (controller == 0) paddBtnCommand_One = newcommand;
-			if (controller == 1) paddBtnCommand_Two = newcommand;
-				if (controller == 2) paddBtnCommand_Three = newcommand;
-					if(controller == 3) paddBtnCommand_Four = newcommand;
+		paddBtnCommand[controller] = newcommand;
 		break;
 	default:
 		break;
--- Exam_Assignment/Minigin/InputManager.h
+++ Exam_Assignment/Minigin/InputManager.h
@@ -28,25 +28,21 @@ namespace dae
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
+		~InputManager();
 		bool ProcessInput(float deltatime);
 		bool IsPressed(ControllerButton button) const;
 		void ChangeCommand(ControllerButton button,int controller, Command* newcommand);
 	private:
 		XINPUT_STATE currentState{};
-		
+		static const int m_MAXammControllers = 4;
 		// command states
-		Command* aBtnCommand_One, *aBtnCommand_Two, *aBtnCommand_Three, *aBtnCommand_Four;
-		Command* bBtnCommand_One, *bBtnCommand_Two, *bBtnCommand_Three, *bBtnCommand_Four;
-		Command* xBtnCommand_One, *xBtnCommand_Two, *xBtnCommand_Three, *xBtnCommand_Four;
-		Command* yBtnCommand_One, *yBtnCommand_Two, *yBtnCommand_Three, *yBtnCommand_Four;
-		Command* padlBtnCommand_One, *padlBtnCommand_Two, *padlBtnCommand_Three, *padlBtnCommand_Four;
-		Command* padrBtnCommand_One, *padrBtnCommand_Two, *padrBtnCommand_Three, *padrBtnCommand_Four;
-		Command* paduBtnCommand_One, *paduBtnCommand_Two, *paduBtnCommand_Three, *paduBtnCommand_Four;
-		Command* paddBtnCommand_One, *paddBtnCommand_Two, *paddBtnCommand_Three, *paddBtnCommand_Four;
-
-		const int m_MAXammControllers = 4;
+		Command* aBtnCommand[m_MAXammControllers];
+		Command* bBtnCommand[m_MAXammControllers];
+		Command* xBtnCommand[m_MAXammControllers];
+		Command* yBtnCommand[m_MAXammControllers];
+		Command* padlBtnCommand[m_MAXammControllers];
+		Command* padrBtnCommand[m_MAXammControllers];
+		Command* paduBtnCommand[m_MAXammControllers];
+		Command* paddBtnCommand[m_MAXammControllers];	
 	};
-
 }
-
-
