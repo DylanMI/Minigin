46a02e30dd9acc2392fef2018eea657d55f00e8e
Laptop Dylan <Dylan.millian@hotmail.com>
2019-08-20 15:58:45 +0200

    Made/Adapted gameObserver, so the rudemantary structure is there

--- Exam_Assignment/Pengo/GameObserver.cpp
+++ Exam_Assignment/Pengo/GameObserver.cpp
@@ -1,11 +1,13 @@
 #include "pch.h"
 #include "GameObserver.h"
 
-
-GameObserver::GameObserver()
+GameObserver::GameObserver(GameObject * gameGridObj)
+	: m_lives { 3 }
+	, m_score { 0 }
+	, m_ammSnoBeeAlive { 0 }
+	, m_isDiamondEventLocked {false}
 {
 }
-
 
 GameObserver::~GameObserver()
 {
@@ -13,4 +15,59 @@ GameObserver::~GameObserver()
 
 void GameObserver::Notify(Event eventType, int extraInfo)
 {
+
+	switch (eventType)
+	{
+	case Event::EVENT_PENGODIED:
+		// reduce lifes
+
+			break;
+	case Event::EVENT_ENEMYDIED:
+		// award points according to extraInfo
+
+		// tell the next egg to hatch
+
+		// keep count of how many sno bees there are
+
+		break;
+	case Event::EVENT_ENEMYSPAWNED:
+		
+		// keep count of how many sno bees there are
+
+
+		break;
+	case Event::EVENT_DIAMONDSCHAIN:
+		// check if it occured yet
+		if (m_isDiamondEventLocked) return;
+		
+		
+		// award points, and lock this event untill reset
+
+		m_isDiamondEventLocked = true;
+
+		break;
+	case Event::EVENT_ENDGAME:
+
+		break;
+
+
+	default:
+		break;
+	}
+
+}
+
+int GameObserver::GetScore()
+{
+	return m_score;
+}
+
+int GameObserver::GetLives()
+{
+	return m_lives;
+}
+
+int GameObserver::GetAmmSnoBeeAlive()
+{
+	return m_ammSnoBeeAlive;
 }
--- Exam_Assignment/Pengo/GameObserver.h
+++ Exam_Assignment/Pengo/GameObserver.h
@@ -5,8 +5,23 @@
 class GameObserver : public Observer
 {
 public:
-	GameObserver();
+	GameObserver(GameObject* gameGridObj);
 	~GameObserver();
 	virtual void Notify(Event eventType, int extraInfo) override;
+
+	int GetScore();
+	int GetLives();
+	
+	int GetAmmSnoBeeAlive();
+
+private:
+	int m_score;
+	int m_lives;
+
+	int m_ammSnoBeeAlive;
+
+	bool m_isDiamondEventLocked;
+
+
 };
 
